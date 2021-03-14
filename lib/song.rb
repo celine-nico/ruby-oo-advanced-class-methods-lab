ed3f10613e66140d2e3d8c8f425367d96a77607e
Cel <celinenico22@gmail.com>
2021-03-14 14:27:54 -0400

    commit tree 838461770688477f548957e016deb123ca554878

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,70 @@ class Song
   end
 
   def save
-    self.class.all << self
+    @@all << self
   end
+
+  def self.create 
+    song = self.new 
+    song.name = name
+    @@all << song
+    song
+  end 
+
+  def Song.new_by_name(name)
+    song = self.new
+    song.name = name  
+    song
+  end 
+
+  def Song.create_by_name(name)
+    song = self.new
+    song.name = name  
+    @@all << song
+    song
+  end 
+
+  def Song.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end 
+
+  def Song.find_or_create_by_name(name)
+    if Song.find_by_name(name) 
+      Song.find_by_name(name)
+    else
+      Song.create_by_name(name)
+    end 
+  end
+
+  def Song.alphabetical
+    @@all.sort_by {|song| song.name}
+  end 
+
+  def Song.new_from_filename(filename)
+    part = filename.split(" - ")
+    artist_name = part[0]
+    song_name = part[1].gsub(".mp3", "")
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+  
+  def Song.create_from_filename(filename)
+    part = filename.split(" - ")
+    artist_name = part[0]
+    song_name = part[1].gsub(".mp3", "")
+
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song.save
+    song
+  end 
+
+  def Song.destroy_all
+    @@all.clear()
+  end 
 
 end
